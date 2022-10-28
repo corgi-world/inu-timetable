192d5b92b47cf590702b8945ea7022a1c2f881a7
corgi-world <developer.msw@gmail.com>
2022-10-28 21:46:02 +0900

    chore: mui 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,33 @@ import GlobalStyles from '../styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import theme from '../styles/theme';
 
-function MyApp({ Component, pageProps }: AppProps) {
+import Head from 'next/head';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '@/styles/createEmotionCache';
+
+const clientSideEmotionCache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}: MyAppProps) {
   return (
-    <>
-      <GlobalStyles />
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
       <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
-    </>
+    </CacheProvider>
   );
 }
 
