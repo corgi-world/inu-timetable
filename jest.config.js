0aba6ca3ca943a712ba125886ecdb5fa94cfd799
corgi-world <developer.msw@gmail.com>
2022-10-29 16:48:31 +0900

    chore: 절대경로에 utils 추가

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ const customJestConfig = {
     '^@/components/(.*)$': '<rootDir>/components/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
     '^@/styles/(.*)$': '<rootDir>/styles/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
 };
