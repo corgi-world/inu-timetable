fde60242fd71c55f97ea3b78dc22868b724c3111
corgi-world <developer.msw@gmail.com>
2022-11-05 12:07:53 +0900

    feat: 로그인 되지 않은 유저 redirect

--- pages/my/index.tsx
+++ pages/my/index.tsx
@@ -8,3 +8,11 @@ import Layout from '@/components/Layout';
 Mypage.getLayout = function getLayout(page: JSX.Element) {
   return <Layout>{page}</Layout>;
 };
+
+import { redirectNotAuthUser } from '@/utils/auth';
+import { NextPageContext } from 'next';
+
+export async function getServerSideProps(context: NextPageContext) {
+  const returnObject = await redirectNotAuthUser(context.req);
+  return returnObject;
+}
