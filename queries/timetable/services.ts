7b47749a1c380655c225cb526b3bd8547021291a
corgi-world <developer.msw@gmail.com>
2022-11-08 23:14:53 +0900

    feat: user timetables get query 구현

--- queries/timetable/services.ts
+++ queries/timetable/services.ts
@@ -3,6 +3,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:3000/api/timetable';
 const MAJOR_MAP_URL = 'majorMap';
 const USER_POST_URL = 'user/post';
+const USER_GET_URL = 'user/get';
 
 export const majorMapService = axios.create({
   baseURL: `${BASE_URL}/${MAJOR_MAP_URL}/`,
@@ -10,4 +11,8 @@ export const majorMapService = axios.create({
 
 export const userPostService = axios.create({
   baseURL: `${BASE_URL}/${USER_POST_URL}/`,
+});
+
+export const userGetService = axios.create({
+  baseURL: `${BASE_URL}/${USER_GET_URL}/`,
 });
