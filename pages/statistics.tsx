8748f71da4c7fadac8dcdeae2678ec0bb68ff23d
corgi-world <developer.msw@gmail.com>
2022-11-09 17:26:48 +0900

    feat: 검색 조건 설정 UI 구현

--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -1,18 +1,63 @@
 import React from 'react';
+import styled from 'styled-components';
+import type { TypeMajorMap } from '@/types/timetable';
 
-export default function Statistics() {
-  return <div>statistics</div>;
+interface IStatistics {
+  semesters: string[];
+  majorMap: TypeMajorMap;
 }
+
+export default function Statistics({ semesters, majorMap }: IStatistics) {
+  const handleSearch = (
+    semester: string,
+    college: string,
+    major: string,
+    grade: string,
+  ) => {
+    console.log(semester, college, major, grade);
+  };
+
+  return (
+    <Wrapper>
+      <ContentsWrapper>
+        <ConditionSelector
+          semesters={semesters}
+          majorMap={majorMap}
+          handleSearch={handleSearch}
+        />
+      </ContentsWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+`;
+
+const ContentsWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  @media (min-width: ${({ theme: { size } }) => size.mobile}) {
+    width: 950px;
+  }
+
+  display: flex;
+  flex-direction: column;
+`;
 
 import Layout from '@/components/Layout';
 Statistics.getLayout = function getLayout(page: JSX.Element) {
   return <Layout>{page}</Layout>;
 };
 
-// import { redirectNotAuthUser } from '@/utils/auth';
-// import { NextPageContext } from 'next';
+import { read } from '@/utils/json';
+import ConditionSelector from '@/components/main/ConditionSelector';
+export async function getStaticProps() {
+  const semesters = read<string[]>('semesters');
+  const majorMap = read<TypeMajorMap>('majorMap');
 
-// export async function getServerSideProps(context: NextPageContext) {
-//   const returnObject = await redirectNotAuthUser(context.req);
-//   return returnObject;
-// }
+  return { props: { semesters, majorMap } };
+}
