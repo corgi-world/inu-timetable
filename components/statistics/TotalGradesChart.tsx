0fe555a2afda5f386b66ff63991b4775b26b7f11
corgi-world <developer.msw@gmail.com>
2022-11-11 14:30:02 +0900

    feat: 전체 학점 chart 컴포넌트 구현

---
+++ components/statistics/TotalGradesChart.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import type { IChartProps } from '@/types/statistics';
+
+import dynamic from 'next/dynamic';
+const ApexChart = dynamic(() => import('react-apexcharts'), { ssr: false });
+import type { ApexOptions } from 'apexcharts';
+
+function TotalGradesChart({ categories, data }: IChartProps) {
+  const xaxis = {
+    ...options.xaxis,
+    categories,
+  };
+
+  return (
+    <ApexChart
+      height={200}
+      type='bar'
+      series={[{ name: '', data }]}
+      options={{ ...options, xaxis }}
+    />
+  );
+}
+
+export default React.memo(TotalGradesChart);
+
+const options: ApexOptions = {
+  dataLabels: {
+    enabled: true,
+    style: { colors: ['black'], fontSize: '15px' },
+    formatter: (v: number) => v + '명',
+  },
+  xaxis: {
+    position: 'bottom',
+    labels: {
+      style: { fontSize: '15px' },
+    },
+  },
+  yaxis: {
+    labels: { show: false },
+  },
+  tooltip: {
+    marker: { show: false },
+    y: {
+      formatter: (v: number) => v + '명',
+    },
+  },
+  colors: ['#ffaa00'],
+  chart: {
+    toolbar: {
+      show: false,
+    },
+    fontFamily: 'Nanum Gothic',
+  },
+};
